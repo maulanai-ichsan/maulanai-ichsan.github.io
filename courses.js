1351d1d4023e9af34de65456a768db628e4943ec
maulanai-ichsan <ichsanachmadmaulana@gmail.com>
2025-01-08 11:00:48 +0700

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,20 @@
+const container = document.getElementsByClassName('container')[0];
+function generateBox(team) {
+    const box = document.createElement('div');
+    box.className = "box"
+    container.appendChild(box)
+    const img = document.createElement('img');
+    img.className = "media"
+    img.src= team.image
+    box.appendChild(img);
+    const title = document.createElement('h4')
+    title.innerText = team.title
+    box.appendChild(title)
+    const path = document.createElement('p')
+    path.innerText = team.path
+    box.appendChild(path)
+}
+
+fetch ('https://fahram.dev/api/v2/courses')
+.then(response => response.json())
+.then(data => data.data.forEach(course => generateBox(course)))
